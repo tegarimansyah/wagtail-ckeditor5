de69a6af3550bc5e89a46cb102c7b8f683a13b4a
Alexandr Goncharenko <8874551+impudentem@users.noreply.github.com>
2020-03-04 12:07:06 +0200

    Update widgets.py

--- wagtail_ckeditor/widgets.py
+++ wagtail_ckeditor/widgets.py
@@ -1,5 +1,3 @@
-
-
 from __future__ import absolute_import, unicode_literals
 
 import json
@@ -7,24 +5,45 @@ import json
 from django.forms import widgets
 from django.utils.safestring import mark_safe
 from wagtail.utils.widgets import WidgetWithScript
-from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
-from wagtail.wagtailcore.rich_text import DbWhitelister
-from wagtail.wagtailcore.rich_text import expand_db_html
+
+try:
+    from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
+    from wagtail.wagtailcore.rich_text import DbWhitelister
+    from wagtail.wagtailcore.rich_text import expand_db_html
+
+    wagtail_version = 1
+
+except ImportError:
+    from wagtail.admin.edit_handlers import RichTextFieldPanel
+    from wagtail.admin.rich_text.converters.editor_html import EditorHTMLConverter
+    from wagtail.core.rich_text import expand_db_html
+    from wagtail.core.rich_text import features
+
+    wagtail_version = 2
 
 from wagtail_ckeditor import settings
 
 
 class CKEditor(WidgetWithScript, widgets.Textarea):
 
+    def __init__(self, *args, **kwargs):
+        if wagtail_version == 2:
+            self.features = features.get_default_features()
+            self.converter = EditorHTMLConverter(self.features)
+
+        super().__init__(*args, **kwargs)
+
     def get_panel(self):
         return RichTextFieldPanel
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, *args, **kwargs):
         if value is None:
             translated_value = None
+        elif wagtail_version == 2:
+            translated_value = expand_db_html(value)
         else:
             translated_value = expand_db_html(value, for_editor=True)
-        return super().render(name, translated_value, attrs)
+        return super().render(name, translated_value, *args, **kwargs)
 
     def render_js_init(self, editor_id, name, value):
 
@@ -34,4 +53,7 @@ class CKEditor(WidgetWithScript, widgets.Textarea):
         original_value = super().value_from_datadict(data, files, name)
         if original_value is None:
             return None
-        return DbWhitelister.clean(original_value)
+        if wagtail_version == 2:
+            return self.converter.to_database_format(original_value)
+        else:
+            return DbWhitelister.clean(original_value)
