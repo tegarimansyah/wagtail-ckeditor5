3be342735fe3c1064d29641836f0e0abf2dc5af0
Alexandr Goncharenko <8874551+impudentem@users.noreply.github.com>
2020-03-10 15:35:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
    name='wagtail-ckeditor',
-   version='0.9',
+   version='1.0',
    description='CKEditor widget for Wagtail CMS',
    author='Martin Mastny',
    author_email='martin.mastny@vscht.cz',
