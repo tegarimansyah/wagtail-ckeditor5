471a920462f27bba9f487108f237a0779485b5b7
tegarimansyah <egar.imansyah@gmail.com>
2021-06-21 08:57:40 +0700

    change version to remove cache (hopefully)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
    name='wagtail-ckeditor5',
-   version='1.0',
+   version='1.0.1',
    description='CKEditor 5 widget for Wagtail CMS',
    author='Martin Mastny',
    author_email='martin.mastny@vscht.cz',
